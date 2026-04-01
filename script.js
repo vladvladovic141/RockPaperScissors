ddfca8650a79ee4930ee405e9fe0d4b41ffa013c
vladovic <vladvladovic141@yandex.ru>
2026-04-01 19:14:18 +0000

    Add basic ui

--- script.js
+++ script.js
@@ -4,27 +4,78 @@ let humanChoice = "";
 let humanScore = 0;
 let computerScore = 0;
 
-playGame();
-function playGame() {
-  for (let i = 0; i < 3; i++) {
-    playRound();
+const container = document.querySelector(".container");
+const btnRock = document.querySelector("#rock");
+btnRock.textContent = "Rock";
+const btnPaper = document.querySelector("#paper");
+btnPaper.textContent = "Paper";
+const btnScissors = document.querySelector("#scissors");
+btnScissors.textContent = "Scissors";
+const btnPanel = document.querySelector(".btnPanel");
+
+const resultsCon = document.createElement("div");
+resultsCon.classList.add("resultsCon");
+const resultsHeader = document.createElement("h2");
+resultsHeader.textContent = "Score";
+//player
+const resultsPlayersCon = document.createElement("div");
+resultsPlayersCon.classList.add("resultsPlayersCon");
+const resultsPlayerCard = document.createElement("div");
+resultsPlayerCard.classList.add("resultsCard");
+const resultsPlayerHeader = document.createElement("h4");
+resultsPlayerHeader.textContent = "Player";
+const resultsPlayerScore = document.createElement("h1");
+resultsPlayerScore.textContent = `${humanScore}`;
+resultsPlayerCard.append(resultsPlayerHeader, resultsPlayerScore);
+resultsPlayersCon.append(resultsPlayerCard);
+// comp
+const resultsComputerCard = document.createElement("div");
+resultsComputerCard.classList.add("resultsCard");
+const resultsComputerHeader = document.createElement("h4");
+resultsComputerHeader.textContent = "Computer";
+const resultsComputerScore = document.createElement("h1");
+resultsComputerScore.textContent = `${computerScore}`;
+resultsComputerCard.append(resultsComputerHeader, resultsComputerScore);
+resultsPlayersCon.append(resultsComputerCard);
+
+
+resultsCon.append(resultsHeader, resultsPlayersCon);
+
+btnPanel.before(resultsCon);
+
+container.addEventListener("click", (e) => {
+  const target = e.target;
+  let selected = "";
+  switch (target.id) {
+    case "rock":
+      selected = "rock";
+      playRound(selected);
+      break;
+    case "paper":
+      selected = "paper";
+      playRound(selected);
+      break;
+    case "scissors":
+      selected = "scissors";
+      playRound(selected);
+      break;
   }
-}
+});
+
+playGame();
+function playGame() {}
 
 function getComputerChoice(array) {
   return array[Math.floor(Math.random() * array.length)];
-}
-function getHumanChoice() {
-  return prompt("Make your choice");
 }
 function printRoundResults() {
   console.log(
     `Player: ${humanChoice}, Computer: ${computerChoice} | ${humanScore} : ${computerScore}`,
   );
 }
-function playRound() {
+function playRound(choice) {
+  humanChoice = choice;
   computerChoice = getComputerChoice(variants);
-  humanChoice = getHumanChoice();
   humanChoice = humanChoice.toLowerCase();
   if (
     (computerChoice === "rock" && humanChoice === "paper") ||
